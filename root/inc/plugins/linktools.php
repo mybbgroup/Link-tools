f3e6dac3f4fba3a66861a7705d3b9554f4486c0e
Laird <lairdshaw77@gmail.com>
2022-01-09 20:07:47 +1000

    Bump version to 1.4.1 proper

--- root/inc/plugins/linktools.php
+++ root/inc/plugins/linktools.php
@@ -154,7 +154,7 @@ function linktools_info() {
 		'website'       => 'https://mybb.group/Thread-Link-Tools',
 		'author'        => 'Laird as a member of the unofficial MyBB Group',
 		'authorsite'    => 'https://mybb.group/User-Laird',
-		'version'       => '1.4.1-devel',
+		'version'       => '1.4.1',
 		// Constructed by converting each digit of 'version' above into two digits (zero-padded if necessary),
 		// then concatenating them, then removing any leading zero(es) to avoid the value being interpreted as octal.
 		'version_code'  => '10401',
