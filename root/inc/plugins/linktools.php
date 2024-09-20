d9c5fefc546857c483924f635fadf36fd4dd1041
Laird <lairdshaw77@gmail.com>
2024-09-20 18:24:11 +1000

    Fix a bug: previews weren't being cleared on rebuilding them

--- root/inc/plugins/linktools.php
+++ root/inc/plugins/linktools.php
@@ -3742,6 +3742,7 @@ function lkt_hookin__admin_tools_recount_rebuild() {
 				// Log admin action
 				log_admin_action($lang->lkt_admin_log_rebuild_linkpreviews);
 				$db->update_query('urls', array('got_preview' => 0));
+				$db->delete_query('url_previews');
 			}
 
 			if (!$mybb->get_input('lkt_linkpreviews_per_page', MyBB::INPUT_INT)) {
